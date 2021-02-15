cded2393d9c497bf86028a0b8a83a964a4727453
Ian Everitt <ian.andrew.everitt@gmail.com>
2021-02-15 18:59:05 -0500

    slight changes to color scheme, added script.js for homepage mobile navbar, modified projects boxes, modified homepage for mobile viewports

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// this is for mobile dropdown navmenu 
+function myFunction() {
+  let x = document.getElementById("mobile-links");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
